b8691e50ccc583188162fa004edbbbc15caaeaaa
Lakshyaraj Purbia <111797833+Hello-WORLD7046@users.noreply.github.com>
2023-12-07 09:40:58 +0530

    Add files via upload

---
+++ process.php
@@ -0,0 +1,117 @@
+<?php
+// Replace these values with your actual database credentials
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "login";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["register"])) {
+        // Process registration form
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        // Perform SQL insert to add user to the database
+        $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+        
+        if ($conn->query($sql) === TRUE) {
+            echo "Registration successful!";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+            echo "Email Already exist";
+        }
+    } elseif (isset($_POST["login"])) {
+        // Process login form
+        $email = $_POST["login_email"];
+        $password = $_POST["login_password"];
+
+        // Perform SQL select to check if user exists in the database
+        $sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            echo "Login successful!";
+        } else {
+            echo "Invalid email or password.";
+        }
+    }
+}
+
+$conn->close();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="Mylogintest.css">
+    <title>Registration</title>
+</head>
+
+
+<body>
+
+    <div class="container" id="container">
+        <div class="form-container sign-up">
+            <form action="process.php" method="POST">
+                <h1>Create Account</h1>
+                <div class="social-icons">
+                    <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-github"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+                <span>or use your email for registration</span>
+                <input type="text" name="name" placeholder="Name" required>
+                <input type="email" name="email" placeholder="Email" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <button type="submit" name="register">Sign Up</button>
+            </form>
+        </div>
+        <div class="form-container sign-in">
+            <form action="process.php" method="POST">
+                <h1>Sign In</h1>
+                <div class="social-icons">
+                    <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-github"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+                <span>or use your email password</span>
+                <input type="email" name="login_email" placeholder="Email" required>
+                <input type="password" name="login_password" placeholder="Password" required>
+                <a href="#">Forget Your Password?</a>
+                <button type="submit" name="login">Sign In</button>
+            </form>
+        </div>
+        <div class="toggle-container">
+            <div class="toggle">
+                <div class="toggle-panel toggle-left">
+                    <h1>Welcome Back!</h1>
+                    <p>Enter your personal details to use all of site features</p>
+                    <button class="hidden" id="login">Sign In</button>
+                </div>
+                <div class="toggle-panel toggle-right">
+                    <h1>Hello, Friend!</h1>
+                    <p>Register with your personal details to use all of site features</p>
+                    <button class="hidden" id="register">Sign Up</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="Myloginscriptest.js"></script>
+</body>
+
+</html>
